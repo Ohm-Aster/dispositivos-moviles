a3d793d9544e94e45e22104f28814fbcf676b7be
OhmAster <om_art1721@hotmail.com>
2022-07-27 13:00:06 -0500

    Add files via upload

---
+++ CalcAreas/java/trapecio.java
@@ -0,0 +1,14 @@
+package com.example.calcareas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class trapecio extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trapecio);
+    }
+}
